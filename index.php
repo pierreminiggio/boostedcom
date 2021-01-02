b75a0d1fcbf4c68df22ef993dc807316db7f2765
Pierre Miniggio <pierre.miniggio@gmail.com>
2021-01-02 17:30:12 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php header('Location: https://jesuisboosted.fr'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Redirection vers le meilleur site des boosted...</title>
+</head>
+<body>
+    <a href="https://jesuisboosted.fr">Si la redirection ne fonctionne pas, cliquez ici</a>.
+</body>
+</html>
